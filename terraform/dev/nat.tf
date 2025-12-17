740b72cb2be907196b7d70368e8b1e7d1821c481
nagendrappa <pnagendrappa@gmail.com>
2025-12-17 18:10:31 +0530

    updated the nat

--- terraform/dev/nat.tf
+++ terraform/dev/nat.tf
@@ -1,9 +1,9 @@
-# resource "aws_nat_gateway" "dpt_nat_gw" {
-#   allocation_id = aws_eip.dpt_nat_eip.id
-#   subnet_id     = aws_subnet.dpt_public_subnet.id
+resource "aws_nat_gateway" "dpt_nat_gw" {
+  allocation_id = aws_eip.dpt_nat_eip.id
+  subnet_id     = aws_subnet.dpt_public_subnet.id
 
-#   tags = {
-#     Name = "dpt_nat_gw"
-#   }
-# }
+  tags = {
+    Name = "dpt_nat_gw"
+  }
+}
 
