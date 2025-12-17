6856838b1fc3e234453885f87f7c1d07ea08ac80
nagendrappa <pnagendrappa@gmail.com>
2025-12-17 18:00:02 +0530

    modified the public route table

--- terraform/dev/private-route-table.tf
+++ terraform/dev/private-route-table.tf
@@ -1,11 +1,12 @@
-resource "aws_route_table" "dpt_private_route_table" {
-    vpc_id = aws_vpc.dpt_vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.dpt_nat_gw.id
-    }
-  
+resource "aws_route_table" "dpt_public_route_table" {
+  vpc_id = aws_vpc.dpt_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpt_igw.id
+  }
+
   tags = {
-    Name = "dpt_private_route_table"
+    Name = "dpt_public_route_table"
   }
 }
