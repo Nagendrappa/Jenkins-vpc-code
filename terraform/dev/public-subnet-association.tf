838c3fc3218e792cbcbca4b6ac1cf1821c054700
nagendrappa <pnagendrappa@gmail.com>
2025-12-18 16:23:19 +0530

    updated the public route table

--- terraform/dev/public-subnet-association.tf
+++ terraform/dev/public-subnet-association.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "dpt5_public_subnet_association" {
-  subnet_id      = aws_subnet.dpt_public_subnet1.id
+  subnet_id      = aws_subnet.dpt_public_subnet_1.id
   route_table_id = aws_route_table.dpt_public_route_table.id
 }
 
